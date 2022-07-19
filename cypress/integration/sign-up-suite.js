fe9ad7d17365581e966da2efe761ce405781ee7a
Ariel Machado <arielma77@gmail.com>
2022-07-18 21:21:18 -0300

    SignUp test case finished
    We can add more validations to the dashboard page tomorrow

--- cypress/integration/sign-up-suite.js
+++ cypress/integration/sign-up-suite.js
@@ -1,6 +1,8 @@
 import { Common } from '../fixtures/page-objects/common-page';
 import { SignUp } from '../fixtures/page-objects/sign-up-page';
 
+// This piece of code was added because of an error in the application
+// --> ReferenceError: grecaptcha is not defined
 Cypress.on('uncaught:exception', (err, runnable) => {
   return false;
 });
@@ -15,7 +17,7 @@ describe('Sign Up automation suite', () => {
     cy.visit('/signup');
   });
 
-  it.only('Sign Up page smoke validations', () => {
+  it('Sign Up page smoke validations', () => {
     // Validate Sign Up page url
     cy.url().should('contains', 'signup');
 
@@ -26,10 +28,14 @@ describe('Sign Up automation suite', () => {
     signUp.getSignUpWithAppleButton().should('be.visible');
     signUp.getOptionTitle().should('be.visible');
 
+    // Input fields
     common.getLabel('Email').should('be.visible');
+    signUp.getEmailInput().should('be.visible');
     common.getLabel('Password').should('be.visible');
-
+    signUp.getPasswordInput().should('be.visible');
     signUp.getPasswordConditionsText().should('have.text', 'Must be at least 8 characters long, contain uppercase letters and a number.')
+
+    signUp.getSignUpButton().should('be.visible');
 
     signUp.getSignUpWithSSOLink().should('be.visible');
     signUp.getHaveAnAccountText().should('be.visible');
@@ -37,6 +43,18 @@ describe('Sign Up automation suite', () => {
   });
 
   it('Sign Up - Manual Sign Up', () => {
+    // Get a random email every time
+    const uuid = () => Cypress._.random(0, 1e8)
+    const id = uuid();
+    const randomEmail = `user${id}@mailinator.com`
 
+    signUp.getEmailInput().type(randomEmail);
+    signUp.getPasswordInput().type('Just4Pass');
+    signUp.getSignUpButton().click();
+
+    // Validate Dashboard page url
+    cy.url().should('contains', 'onboarding/downloads');
+
+    //TODO: Add more validations
   });
 })
